7e4a1559fd266118503d89b810bf6fc02cdd7b2e
Ram Prakash T <trrramprakash@gmail.com>
2025-02-24 20:07:25 +0530

    Initial project push

---
+++ src/main/java/com/feedback/feedbackCollection/FeedbackCollectionApplication.java
@@ -0,0 +1,13 @@
+package com.feedback.feedbackCollection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedbackCollectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedbackCollectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/feedback/feedbackCollection/FeedbackCollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feedback.feedbackCollection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedbackCollectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
